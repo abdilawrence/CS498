cd0e84f634307c06253b41c7660fe49b56d2cfef
abdilawrence <60329772+abdilawrence@users.noreply.github.com>
2022-04-11 11:45:07 -0500

    Update sparktest.py

--- HW3Code/sparktest.py
+++ HW3Code/sparktest.py
@@ -1,9 +1,9 @@
-
 import findspark
 findspark.init()
 
 import pyspark
 import sys
+import json
 
 if len(sys.argv) != 3:
         raise Exception("Exactly 2 arguments are required: <inputUri> <outputUri>")
@@ -12,26 +12,27 @@ inputUri=sys.argv[1]
 outputUri=sys.argv[2]
 
 def myMapFunc(x): # takes an input, provides an output pairing
-        return (len(x) - x.count(" "), 1)
+        return (len(x), 1)
 
 def myReduceFunc(v1, v2): # Merge two values with a common key - operation must be assoc. and commut.
         return v1 + v2
 
 sc = pyspark.SparkContext()
-print("Spark Context initialized.")
+
 # textFile --> take the address of a text file, return it as an RDD (hadoop dataset) of strings
 lines = sc.textFile(sys.argv[1])
+
 # Flatmap --> Apply a function to each element of the dataset, then flatten the result.
 words = lines.flatMap(lambda line: line.split("\n"))
 wordCounts = words.map(myMapFunc).reduceByKey(myReduceFunc)
-print("Operations complete.")
-wordCounts.saveAsTextFile(sys.argv[2])
-print("Output saved as text file.")
-# type(wordCounts)
+
 # sys.argv[0] sparktest.py
 # sys.argv[1] input.txt
 # sys.argv[2] output
 
 countDict = wordCounts.collectAsMap()
-print(countDict)
-# print(type(countDict))
+
+def getJSON():
+        return json.dumps(countDict, sort_keys = True)
+
+print(getJSON())
